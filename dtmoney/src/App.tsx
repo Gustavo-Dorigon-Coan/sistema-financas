3f0d915108a72a481c15692630048ef0368a4ecc
Gustavo-Dorigon-Coan <gustavodorigonpessoal@gmail.com>
2022-07-12 20:47:21 -0300

    Comit Inicial - Pratica

---
+++ dtmoney/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button, Input, Title } from "./components/Title";
+
+export function App() {
+  return (
+    <div className="App">
+      <Title>Hello World</Title>
+      <Input></Input>
+      <Button> Salvar </Button>
+    </div>
+  );
+}
---
+++ dtmoney/src/components/Title.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 64px;
+  color: blue;
+`
+
+
+export const Button = styled.button`
+  font-size: 50px;
+  color: red;
+  background-color: black;
+`
+
+
+export const Input = styled.input`
+  font-size: 50px;
+  color: purple;
+  display: flex;
+`
